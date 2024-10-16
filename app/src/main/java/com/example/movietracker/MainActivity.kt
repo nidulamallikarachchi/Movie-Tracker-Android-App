67a954f8ac3f2539398f05595eafc39e77be462e
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-16 23:36:21 +1100

    Testing Firebase Connection

--- app/src/main/java/com/example/movietracker/MainActivity.kt
+++ app/src/main/java/com/example/movietracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.movietracker
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseFirestore.getInstance()
+        val testButton: Button = findViewById(R.id.testFirebaseButton)
+        testButton.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
+            val testMovie = hashMapOf(
+                "title" to "Test Movie",
+                "release_date" to "2024"
+            )
+
+            db.collection("movies")
+                .add(testMovie)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("FirebaseTest", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("FirebaseTest", "Error adding document", e)
+                }
+        }
     }
 }
