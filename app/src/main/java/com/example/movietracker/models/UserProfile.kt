62da6b5cff67d62296efef30f9f37eda8ff2116a
Nidula Mallikarachchi <nidulamallikarachchi@gmail.com>
2024-10-21 20:11:33 +1100

    Final Commit

--- app/src/main/java/com/example/movietracker/models/UserProfile.kt
+++ app/src/main/java/com/example/movietracker/models/UserProfile.kt
@@ -1,12 +1,12 @@
 package com.example.movietracker.models
 
 data class UserProfile(
-    val userId: String = "",             // Default value for userId
-    val firstName: String = "",          // Default value for firstName
-    val username: String = "",            // Default value for username
-    val aboutMe: String = "",             // Default value for aboutMe
-    val profilePicUrl: String = "",       // Default value for profilePicId
-    val moviePreferences: List<String> = listOf() // Default value for moviePreferences
+    val userId: String = "",
+    val firstName: String = "",
+    val username: String = "",
+    val aboutMe: String = "",
+    val profilePicUrl: String = "",
+    val moviePreferences: List<String> = listOf()
 ) {
     // No-argument constructor required for Firestore
     constructor() : this("", "", "", "", "", listOf())
